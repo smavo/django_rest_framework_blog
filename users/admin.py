ec755ee4a8ad8aef9dfc8b779539030c07f1e558
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-09-29 09:26:55 -0500

    Add Campos adicionales al modelo User, Add administracion de Modelo Usuario en el administrador Django

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users.models import User
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    #
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Información Personal', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Redes Sociales', {'fields': ('web_site', 'github')}),
+    )
+
--- users/models.py
+++ users/models.py
@@ -4,5 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    web_site = models.CharField(max_length=255, blank=True)
+    github = models.CharField(max_length=255, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
