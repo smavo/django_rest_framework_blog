ddea03282b1a2c994ed9780501a34dcce58054e8
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-09-29 18:33:17 -0500

    Add conexion con Postgres, Configuracion de variables de entorno

--- Notas.md
+++ Notas.md
@@ -4,20 +4,26 @@
 ## Crear entorno virtual para el proyecto
 ```python -m venv venv```
 
+
 ## Install Django 4.2.4
 ```pip install django==4.2.4```
+
 
 ## Crear proyecto en Django en la carpeta raiz
 ```django-admin startproject <nombre_proyecto> . ```
 
+
 ## Crear nuevo app
 ```python manage.py startapp <app_name>```
+
 
 ## Add Django Rest Framework
 ```pip install djangorestframework```
 
+
 ## Add drf-yasg
 ```pip install drf-yasg```
+
 
 ## Renderizar archivos estaticos
 ```python manage.py collectstatic```
@@ -29,5 +35,23 @@
 * 3. Crear el modelo User por defecto
 * 4. Volver a correr las migraciones
 
+
 ## Crear usuario
 ```python manage.py createsuperuser```
+
+
+## Add decouple - Para la separación estricta de la configuración de código
+https://pypi.org/project/python-decouple/ 
+```pip install python-decouple```
+
+
+## Add psycogp2 para conectar base de datos Postgres
+https://pypi.org/project/psycopg2/ 
+```pip install psycopg2```
+
+
+## Add Dumpdata
+```python manage.py dumpdata > api_blog.json```
+
+## Load Dumpdata
+```python manage.py loaddata api_blog.json```
--- blog_django/settings.py
+++ blog_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import datetime
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hf-%@o3gus+$h0=s-%6l_*@m11*l^wb_nq%n9mstre8fqau_ck'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'blog_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT')
     }
 }
 
